918c4eb0efd6456169238e2d0f2453d0094dd14a
capricorn <capricorn@basebandit.org>
2020-06-20 23:35:19 +0300

    model: define a url schema

---
+++ internal/store/model/url.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//URL contains all the related info about the shortened url.
+type URL struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty"`
+	User         primitive.ObjectID `bson:"user,omitempty"`
+	OriginalURL  string             `bson:"originalURL,omitempty"`
+	ShortenedURL string             `bson:"shortenedURL,omitempty"`
+	CustomAlias  string             `bson:"customAlias,omitempty"`
+	VisitCount   int                `bson:"visitCount,omitempty"`
+	Target       string             `bson:"target,omitempty"`
+	CreatedAt    time.Time          `bson:"createdAt,omitempty"`
+	UpdatedAt    time.Time          `bson:"updatedAt,omitempty"`
+}
