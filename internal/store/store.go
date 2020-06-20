f6038c981e1ce4f5648f6a7bf6b84eba0bbf8131
capricorn <capricorn@basebandit.org>
2020-06-20 23:23:46 +0300

    store: define fupisha data store interface

---
+++ internal/store/store.go
@@ -0,0 +1,22 @@
+package store
+
+import "github.com/nairobi-gophers/fupisha/internal/store/model"
+
+//Store is a data store interface.
+type Store interface {
+	Users() UserStore
+	Urls() URLStore
+}
+
+//UserStore is a user data store interface.
+type UserStore interface {
+	New(name, email, password string) (interface{}, error)
+	Get(id string) (model.User, error)
+	GetByEmail(email string) (model.User, error)
+}
+
+//URLStore is a url data store interface.
+type URLStore interface {
+	New(userID, originalURL, shortenedURL, customAlias, target string) error
+	Get(id int) (model.URL, error)
+}
