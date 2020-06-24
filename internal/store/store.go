baf1f9cfce8a0fbf8ee93780b10c5b21d190f31c
capricorn <capricorn@basebandit.org>
2020-06-24 07:54:24 +0300

    change methd signature to reflect the new return type

--- internal/store/store.go
+++ internal/store/store.go
@@ -3,6 +3,7 @@ package store
 import (
 	"github.com/gofrs/uuid"
 	"github.com/nairobi-gophers/fupisha/internal/store/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //Store is a data store interface.
@@ -13,7 +14,7 @@ type Store interface {
 
 //UserStore is a user data store interface.
 type UserStore interface {
-	New(name, email, password string) (interface{}, error)
+	New(name, email, password string) (primitive.ObjectID, error)
 	Get(id string) (model.User, error)
 	GetByEmail(email string) (model.User, error)
 	SetAPIKey(id string, key uuid.UUID) (model.User, error)
