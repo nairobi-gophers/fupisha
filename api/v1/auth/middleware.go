389aa560e733b888259cf2f6f1a8406a064e47f7
capricorn <capricorn@basebandit.org>
2020-07-07 22:22:15 +0300

    refactor: swap zap logger with sirupsen logger

--- api/v1/auth/middleware.go
+++ api/v1/auth/middleware.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/go-chi/render"
 	"github.com/nairobi-gophers/fupisha/internal/auth"
-	"github.com/nairobi-gophers/fupisha/internal/logging"
 )
 
 //Verifier http middleware will verify a jwt string from a http request.
@@ -16,7 +15,7 @@ func (rs *Resource) Verifier(next http.Handler) http.Handler {
 		if r.Header["Authorization"] != nil {
 			authHeader := r.Header.Get("Authorization")
 			if len(authHeader) < 7 || strings.ToUpper(authHeader[:6]) != "BEARER" {
-				logging.FromContext(r.Context()).Error(ErrLoginToken)
+				log(r).Error(ErrLoginToken)
 				render.Render(w, r, ErrUnauthorized(ErrLoginToken))
 				return
 			}
@@ -25,18 +24,18 @@ func (rs *Resource) Verifier(next http.Handler) http.Handler {
 			secret := hex.EncodeToString([]byte(rs.Config.JWT.Secret))
 			service, err := auth.NewService(secret)
 			if err != nil {
-				logging.FromContext(r.Context()).Error(err)
+				log(r).WithField("secret", secret).Error(err)
 				render.Render(w, r, ErrInternalServerError)
 				return
 			}
 			_, _, err = service.Decode(token)
 			if err != nil {
-				logging.FromContext(r.Context()).Error(err)
+				log(r).WithField("token", token).Error(err)
 				render.Render(w, r, ErrUnauthorized(ErrLoginToken))
 				return
 			}
 		} else {
-			logging.FromContext(r.Context()).Error(ErrMissingToken)
+			log(r).Error(ErrMissingToken)
 			render.Render(w, r, ErrUnauthorized(ErrMissingToken))
 			return
 		}
