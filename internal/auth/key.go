4a0988f11f6328c312023eb34b7c6b9b01612603
capricorn <capricorn@basebandit.org>
2020-07-07 19:32:04 +0300

    generate unique api key

---
+++ internal/auth/key.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"log"
+
+	"github.com/nairobi-gophers/fupisha/internal/encoding"
+	"github.com/nairobi-gophers/fupisha/internal/store"
+)
+
+//GenAPIKey generates an api key for third party applications.
+func GenAPIKey(uid string, s store.Store) (string, error) {
+	key := encoding.GenUniqueID()
+
+	//persist the api key to the database before encoding it.
+	_, err := s.Users().SetAPIKey(uid, key)
+
+	if err != nil {
+		log.Fatalf("failed to persist generated api key: %s", err)
+	}
+	//encode the api key and return it to the caller.
+	return encoding.Encode(key), nil
+}
