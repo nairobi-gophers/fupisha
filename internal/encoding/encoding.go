9889bc062de9627ba8d97f23a40dd4b5c53cd48e
capricorn <capricorn@basebandit.org>
2020-07-07 19:24:48 +0300

    decode a base64 string to raw uuid

--- internal/encoding/encoding.go
+++ internal/encoding/encoding.go
@@ -1,8 +1,28 @@
 package encoding
 
-import "github.com/gofrs/uuid"
+import (
+	b64 "encoding/base64"
+
+	"github.com/gofrs/uuid"
+)
 
 //Encode encodes the uuid to a base64 string that is url-safe.
 func Encode(id uuid.UUID) string {
 	return b64.RawURLEncoding.EncodeToString(id.Bytes())
+}
+
+//Decode decodes a base64 string to a raw uuid.
+func Decode(id string) (uuid.UUID, error) {
+	dec, err := b64.RawURLEncoding.DecodeString(id)
+
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	decoded, err := uuid.FromBytes(dec)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return decoded, nil
 }
