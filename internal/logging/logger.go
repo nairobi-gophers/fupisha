27237b8187bde9a61d0048fdbb77e854081f400a
capricorn <capricorn@basebandit.org>
2020-07-07 22:17:30 +0300

    log api calls

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -1,37 +1,145 @@
+//Package logging provides logging capability for mvuvi api.
 package logging
 
 import (
-	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os/user"
+	"time"
 
-	"go.uber.org/zap"
+	"github.com/go-chi/chi/middleware"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-type loggerKey struct{}
+var (
+	//Logger is a configured logrus.Logger
+	Logger *logrus.Logger
+)
 
-var fallbackLogger *zap.SugaredLogger
+//StructuredLogger is a structured logrus Logger.
+type StructuredLogger struct {
+	Logger *logrus.Logger
+}
 
-func init() {
-	config := zap.NewProductionConfig()
-	config.EncoderConfig.MessageKey = "message"
-	config.EncoderConfig.LevelKey = "severity"
-	config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+//NewLogger creates and configures a new logrus Logger.
+func NewLogger() *logrus.Logger {
+	Logger = logrus.New()
+	if viper.GetBool("MVUVI_TEXT_LOGGING") {
+		Logger.Formatter = &logrus.TextFormatter{
+			DisableTimestamp: true,
+		}
+	}
 
-	if logger, err := config.Build(); err != nil {
-		fallbackLogger = zap.NewNop().Sugar()
-	} else {
-		fallbackLogger = logger.Named("default").Sugar()
+	level := viper.GetString("MVUVI_LOG_LEVEL")
+	if level == "" {
+		level = "error"
+	}
+	l, err := logrus.ParseLevel(level)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Logger.Level = l
+
+	usr, err := user.Current()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := usr.HomeDir + "/.mvuvi/mvuvi.log"
+
+	Logger.SetOutput(&lumberjack.Logger{
+		Filename:   path,
+		MaxSize:    500, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28,   //days
+		Compress:   true, // disabled by default
+	})
+
+	return Logger
+}
+
+//NewStructuredLogger implements a custom structured logrus logger.
+func NewStructuredLogger(logger *logrus.Logger) func(next http.Handler) http.Handler {
+	return middleware.RequestLogger(&StructuredLogger{Logger})
+}
+
+//NewLogEntry sets default request log fields.
+func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
+	entry := &StructuredLoggerEntry{Logger: logrus.NewEntry(l.Logger)}
+	logFields := logrus.Fields{}
+
+	logFields["ts"] = time.Now().UTC().Format(time.RFC1123)
+	if reqID := middleware.GetReqID(r.Context()); reqID != "" {
+		logFields["req_id"] = reqID
+	}
+
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+
+	logFields["http_scheme"] = scheme
+	logFields["http_proto"] = r.Proto
+	logFields["http_method"] = r.Method
+
+	logFields["remote_addr"] = r.RemoteAddr
+	logFields["user_agent"] = r.UserAgent()
+
+	logFields["uri"] = fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)
+	logFields["uri"] = fmt.Sprintf("%s", r.RequestURI)
+
+	entry.Logger = entry.Logger.WithFields(logFields)
+	entry.Logger.Infoln("request started")
+
+	return entry
+}
+
+//StructuredLoggerEntry is a logrus.FieldLogger
+type StructuredLoggerEntry struct {
+	Logger logrus.FieldLogger
+}
+
+func (l *StructuredLoggerEntry) Write(status, bytes int, req http.Header, elapsed time.Duration, data interface{}) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"resp_status":       status,
+		"resp_bytes_length": bytes,
+		"resp_elapsed_ms":   float64(elapsed.Nanoseconds()) / 1000000.0,
+	})
+
+	l.Logger.Infoln("request complete")
+}
+
+//Panic prints stack trace
+func (l *StructuredLoggerEntry) Panic(v interface{}, stack []byte) {
+	l.Logger = l.Logger.WithFields(logrus.Fields{
+		"stack": string(stack),
+		"panic": fmt.Sprintf("%+v", v),
+	})
+}
+
+// Helper methods used by the application to get the request-scoped
+// logger entry and set additional fields between handlers.
+
+// GetLogEntry return the request scoped logrus.FieldLogger.
+func GetLogEntry(r *http.Request) logrus.FieldLogger {
+	entry := middleware.GetLogEntry(r).(*StructuredLoggerEntry)
+	return entry.Logger
+}
+
+//LogEntrySetField adds a field to the request scoped logrus.FieldLogger.
+func LogEntrySetField(r *http.Request, key string, value interface{}) {
+	if entry, ok := r.Context().Value(middleware.LogEntryCtxKey).(*StructuredLoggerEntry); ok {
+		entry.Logger = entry.Logger.WithField(key, value)
 	}
 }
 
-//WithLogger returns a logger object inside a context
-func WithLogger(ctx context.Context, logger *zap.SugaredLogger) context.Context {
-	return context.WithValue(ctx, loggerKey{}, logger)
-}
-
-//FromContext retrieves a logger object for use from a context
-func FromContext(ctx context.Context) *zap.SugaredLogger {
-	if logger, ok := ctx.Value(loggerKey{}).(*zap.SugaredLogger); ok {
-		return logger
+//LogEntrySetFields adds multiple fields to the request scoped logrus.FieldLogger
+func LogEntrySetFields(r *http.Request, fields map[string]interface{}) {
+	if entry, ok := r.Context().Value(middleware.LogEntryCtxKey).(*StructuredLoggerEntry); ok {
+		entry.Logger = entry.Logger.WithFields(fields)
 	}
-	return fallbackLogger
 }
