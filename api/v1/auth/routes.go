de63521c0f0c5d9915d68976f0f583431560908f
capricorn <capricorn@basebandit.org>
2020-07-09 20:39:31 +0300

    refactor: rename signup route handler

--- api/v1/auth/routes.go
+++ api/v1/auth/routes.go
@@ -5,6 +5,6 @@ import "github.com/go-chi/chi"
 //Router provides necessary routes for password restricted authentication flow.
 func (rs *Resource) Router() *chi.Mux {
 	r := chi.NewRouter()
-	r.Post("/signup", rs.handleSignup)
+	r.Post("/signup", rs.HandleSignup)
 	return r
 }
