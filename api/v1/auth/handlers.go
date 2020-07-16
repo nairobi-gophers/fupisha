6090a5003a4be3be5b5dcd70b84b95b91c070dc8
capricorn <capricorn@basebandit.org>
2020-07-16 21:53:32 +0300

    add login handler

--- api/v1/auth/handlers.go
+++ api/v1/auth/handlers.go
@@ -9,12 +9,18 @@ import (
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
 	"github.com/nairobi-gophers/fupisha/internal/logging"
+	"github.com/nairobi-gophers/fupisha/internal/provider"
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type signupRequest struct {
 	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type loginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
@@ -26,6 +32,13 @@ func (body *signupRequest) Bind(r *http.Request) error {
 
 	return validation.ValidateStruct(body, validation.Field(&body.Email, validation.Required, is.Email), validation.Field(&body.Name, validation.Required, validation.Length(3, 32), is.ASCII),
 		validation.Field(&body.Password, validation.Required, validation.Length(8, 32), is.Alphanumeric))
+}
+
+func (body *loginRequest) Bind(r *http.Request) error {
+	body.Email = strings.TrimSpace(body.Email)
+	body.Password = strings.TrimSpace(body.Password)
+
+	return validation.ValidateStruct(body, validation.Field(&body.Email, validation.Required, is.Email), validation.Field(&body.Password, validation.Required, validation.Length(8, 32), is.Alphanumeric))
 }
 
 //HandleSignup signup handler func for handling requests for new accounts.
@@ -60,6 +73,59 @@ func (rs Resource) HandleSignup(w http.ResponseWriter, r *http.Request) {
 
 	render.Status(r, http.StatusCreated)
 	render.Respond(w, r, http.NoBody)
+}
+
+//HandleLogin login handler for handling login requests
+func (rs Resource) HandleLogin(w http.ResponseWriter, r *http.Request) {
+
+	body := loginRequest{}
+
+	if err := render.Bind(r, &body); err != nil {
+		log(r).Error(err)
+		render.Render(w, r, ErrInvalidRequest(err))
+		return
+	}
+
+	usr, err := rs.Store.Users().GetByEmail(body.Email)
+	if err != nil {
+		log(r).Error(err)
+		render.Render(w, r, ErrUnauthorized(ErrInvalidEmailOrPassword))
+		return
+	}
+
+	if _, err := usr.Compare(usr.Password, body.Password); err != nil {
+		log(r).WithField("password", body.Password).Error(err)
+		render.Render(w, r, ErrUnauthorized(ErrInvalidEmailOrPassword))
+		return
+	}
+
+	jwtService, err := provider.NewJWTService(rs.Config.JWT.Secret)
+	if err != nil {
+		log(r).Error(err)
+		render.Render(w, r, ErrInternalServerError)
+		return
+	}
+
+	token, err := jwtService.Encode(usr.ID.Hex())
+	if err != nil {
+		log(r).Error(err)
+		render.Render(w, r, ErrInternalServerError)
+		return
+	}
+
+	resBody := struct {
+		Email  string `json:"email"`
+		Name   string `json:"name"`
+		UserID string `json:"id"`
+		Token  string `json:"token"`
+	}{
+		Email:  usr.Email,
+		Name:   usr.Name,
+		UserID: usr.ID.Hex(),
+		Token:  token,
+	}
+
+	render.Respond(w, r, &resBody)
 }
 
 func log(r *http.Request) logrus.FieldLogger {
