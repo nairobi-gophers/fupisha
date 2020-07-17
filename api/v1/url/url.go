37258753547f89149350b2f5257f23815290f21b
capricorn <capricorn@basebandit.org>
2020-07-17 17:59:52 +0300

    export url resource constructor

---
+++ api/v1/url/url.go
@@ -0,0 +1,20 @@
+package url
+
+import (
+	"github.com/nairobi-gophers/fupisha/internal/config"
+	"github.com/nairobi-gophers/fupisha/internal/store"
+)
+
+//Resource defines dependencies for url handlers.
+type Resource struct {
+	Store  store.Store
+	Config *config.Config
+}
+
+//NewResource returns a configures url resource.
+func NewResource(store store.Store, cfg *config.Config) *Resource {
+	return &Resource{
+		Store:  store,
+		Config: cfg,
+	}
+}
