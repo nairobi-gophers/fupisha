c32320ebf7421d089896eb809b062c15ca397c35
capricorn <capricorn@basebandit.org>
2020-07-17 14:54:28 +0300

    extract userID from context

--- api/v1/auth/auth.go
+++ api/v1/auth/auth.go
@@ -1,11 +1,11 @@
 package auth
 
 import (
+	"context"
+
 	"github.com/nairobi-gophers/fupisha/internal/config"
 	"github.com/nairobi-gophers/fupisha/internal/store"
 )
-
-
 
 //Resource defines dependencies for auth handlers.
 type Resource struct {
@@ -19,4 +19,10 @@ func NewResource(store store.Store, cfg *config.Config) *Resource {
 		Store:  store,
 		Config: cfg,
 	}
+}
+
+//FromContext extracts user id from a Context
+func FromContext(ctx context.Context) (string, bool) {
+	userID, ok := ctx.Value(userIDKey).(string)
+	return userID, ok
 }
