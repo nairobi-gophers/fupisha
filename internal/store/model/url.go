447e02b55503fb3ec64a0305019a97fb40be6746
basebandit <evansonmwangi83@gmail.com>
2020-12-31 12:37:15 +0300

    add struct tags for sqlx serialization

--- internal/store/model/url.go
+++ internal/store/model/url.go
@@ -2,17 +2,15 @@ package model
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //URL contains all the related info about the shortened url.
 type URL struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty"`
-	User         primitive.ObjectID `bson:"user,omitempty"`
-	OriginalURL  string             `bson:"originalURL,omitempty"`
-	ShortenedURL string             `bson:"shortenedURL,omitempty"`
-	VisitCount   int                `bson:"visitCount,omitempty"`
-	CreatedAt    time.Time          `bson:"createdAt,omitempty"`
-	UpdatedAt    time.Time          `bson:"updatedAt,omitempty"`
+	ID           string    `bson:"_id,omitempty" db:"id"`
+	User         string    `bson:"user,omitempty" db:"user"`
+	OriginalURL  string    `bson:"originalURL,omitempty" db:"originalURL"`
+	ShortenedURL string    `bson:"shortenedURL,omitempty" db:"shortenedURL"`
+	VisitCount   int       `bson:"visitCount,omitempty" db:"visitCount"`
+	CreatedAt    time.Time `bson:"createdAt,omitempty" db:"createdAt"`
+	UpdatedAt    time.Time `bson:"updatedAt,omitempty" db:"updatedAt"`
 }
