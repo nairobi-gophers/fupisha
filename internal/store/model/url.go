505fcd8da3423b0500341fd4ade57c55b63608fc
basebandit <evansonmwangi83@gmail.com>
2020-12-31 13:30:41 +0300

    rename db struct tags

--- internal/store/model/url.go
+++ internal/store/model/url.go
@@ -8,9 +8,9 @@ import (
 type URL struct {
 	ID           string    `bson:"_id,omitempty" db:"id"`
 	User         string    `bson:"user,omitempty" db:"user"`
-	OriginalURL  string    `bson:"originalURL,omitempty" db:"originalURL"`
-	ShortenedURL string    `bson:"shortenedURL,omitempty" db:"shortenedURL"`
-	VisitCount   int       `bson:"visitCount,omitempty" db:"visitCount"`
-	CreatedAt    time.Time `bson:"createdAt,omitempty" db:"createdAt"`
-	UpdatedAt    time.Time `bson:"updatedAt,omitempty" db:"updatedAt"`
+	OriginalURL  string    `bson:"originalURL,omitempty" db:"original_url"`
+	ShortenedURL string    `bson:"shortenedURL,omitempty" db:"short_url"`
+	VisitCount   int       `bson:"visitCount,omitempty" db:"visit_count"`
+	CreatedAt    time.Time `bson:"createdAt,omitempty" db:"created_at"`
+	UpdatedAt    time.Time `bson:"updatedAt,omitempty" db:"updated_at"`
 }
