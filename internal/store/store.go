656809086a9e6257df9a1d596e8fe0eecbdcdd34
basebandit <evansonmwangi83@gmail.com>
2020-12-31 09:18:04 +0300

    refactor store methods signature to accept context object as the first param

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"context"
+
 	"github.com/gofrs/uuid"
 	"github.com/nairobi-gophers/fupisha/internal/store/model"
 )
@@ -13,14 +15,14 @@ type Store interface {
 
 //UserStore is a user data store interface.
 type UserStore interface {
-	New(name, email, password string) (string, error)
-	Get(id string) (model.User, error)
-	GetByEmail(email string) (model.User, error)
-	SetAPIKey(id string, key uuid.UUID) error
+	New(ctx context.Context, name, email, password string) (string, error)
+	Get(ctx context.Context, id string) (model.User, error)
+	GetByEmail(ctx context.Context, email string) (model.User, error)
+	SetAPIKey(ctx context.Context, id string, key uuid.UUID) error
 }
 
 //URLStore is a url data store interface.
 type URLStore interface {
-	New(userID, originalURL, shortenedURL string) (string, error)
-	Get(id string) (model.URL, error)
+	New(ctx context.Context, userID, originalURL, shortenedURL string) (string, error)
+	Get(ctx context.Context, id string) (model.URL, error)
 }
