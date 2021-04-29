1f53b3617068f6ad0a8f44e1a23cb90f0bea70e6
basebandit <evansonmwangi83@gmail.com>
2021-04-29 16:49:26 +0300

    remove bson tags

--- internal/store/url_model.go
+++ internal/store/url_model.go
@@ -1,16 +1,18 @@
-package model
+package store
 
 import (
 	"time"
+
+	"github.com/gofrs/uuid"
 )
 
 //URL contains all the related info about the shortened url.
 type URL struct {
-	ID           string    `bson:"_id,omitempty" db:"id"`
-	Owner        string    `bson:"user,omitempty" db:"owner"`
-	OriginalURL  string    `bson:"originalURL,omitempty" db:"original_url"`
-	ShortenedURL string    `bson:"shortenedURL,omitempty" db:"short_url"`
-	VisitCount   int       `bson:"visitCount,omitempty" db:"visit_count"`
-	CreatedAt    time.Time `bson:"createdAt,omitempty" db:"created_at"`
-	UpdatedAt    time.Time `bson:"updatedAt,omitempty" db:"updated_at"`
+	ID           string    `db:"id"`
+	Owner        uuid.UUID `db:"owner"`
+	OriginalURL  string    `db:"original_url"`
+	ShortenedURL string    `db:"short_url"`
+	VisitCount   *int      `db:"visit_count,omitempty"`
+	CreatedAt    time.Time `db:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at"`
 }
