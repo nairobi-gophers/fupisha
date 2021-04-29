4d5c129cb9ca5890a9d925fc129a48acb09e43c4
basebandit <evansonmwangi83@gmail.com>
2021-04-29 17:44:53 +0300

    refactor

--- api/v1/auth/handlers.go
+++ api/v1/auth/handlers.go
@@ -2,21 +2,19 @@ package auth
 
 import (
 	"encoding/hex"
-	"errors"
 	"net/http"
 	"strings"
 
 	"github.com/go-chi/render"
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
+	"github.com/lib/pq"
 	"github.com/nairobi-gophers/fupisha/internal/logging"
 	"github.com/nairobi-gophers/fupisha/internal/provider"
 	"github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type signupRequest struct {
-	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
@@ -29,10 +27,8 @@ type loginRequest struct {
 func (body *signupRequest) Bind(r *http.Request) error {
 	body.Email = strings.TrimSpace(body.Email)
 	body.Password = strings.TrimSpace(body.Password)
-	body.Name = strings.TrimSpace(body.Name)
 
-	return validation.ValidateStruct(body, validation.Field(&body.Email, validation.Required, is.Email), validation.Field(&body.Name, validation.Required, validation.Length(3, 32), is.ASCII),
-		validation.Field(&body.Password, validation.Required, validation.Length(8, 32), is.Alphanumeric))
+	return validation.ValidateStruct(body, validation.Field(&body.Email, validation.Required, is.Email), validation.Field(&body.Password, validation.Required, validation.Length(8, 32), is.Alphanumeric))
 }
 
 func (body *loginRequest) Bind(r *http.Request) error {
@@ -53,18 +49,16 @@ func (rs Resource) HandleSignup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err := rs.Store.Users().New(body.Name, body.Email, body.Password)
+	_, err := rs.Store.Users().New(r.Context(), body.Email, body.Password)
 
 	if err != nil {
-		var e mongo.WriteException
-		if errors.As(err, &e) {
+
+		if pqErr, ok := err.(*pq.Error); ok {
 			//If its a unique key violation
-			for _, we := range e.WriteErrors {
-				if we.Code == 11000 {
-					log(r).WithField("email", body.Email).Error(err)
-					render.Render(w, r, ErrDuplicateField(ErrEmailTaken))
-					return
-				}
+			if pqErr.Code == pq.ErrorCode("23505") {
+				log(r).WithField("email", body.Email).Error(err)
+				render.Render(w, r, ErrDuplicateField(ErrEmailTaken))
+				return
 			}
 		}
 		log(r).WithField("email", body.Email).Error(err)
@@ -87,7 +81,7 @@ func (rs Resource) HandleLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	usr, err := rs.Store.Users().GetByEmail(body.Email)
+	usr, err := rs.Store.Users().GetByEmail(r.Context(), body.Email)
 	if err != nil {
 		log(r).Error(err)
 		render.Render(w, r, ErrUnauthorized(ErrInvalidEmailOrPassword))
@@ -108,7 +102,7 @@ func (rs Resource) HandleLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token, err := jwtService.Encode(usr.ID.Hex())
+	token, err := jwtService.Encode(usr.ID.String())
 	if err != nil {
 		log(r).Error(err)
 		render.Render(w, r, ErrInternalServerError)
@@ -117,13 +111,11 @@ func (rs Resource) HandleLogin(w http.ResponseWriter, r *http.Request) {
 
 	resBody := struct {
 		Email  string `json:"email"`
-		Name   string `json:"name"`
 		UserID string `json:"id"`
 		Token  string `json:"token"`
 	}{
 		Email:  usr.Email,
-		Name:   usr.Name,
-		UserID: usr.ID.Hex(),
+		UserID: usr.ID.String(),
 		Token:  token,
 	}
 
