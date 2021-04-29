9ad1d92b16a1eef10ec0e92081797718c95041ba
basebandit <evansonmwangi83@gmail.com>
2021-04-29 17:20:58 +0300

    cleanup

--- internal/store/postgres/store_test.go
+++ internal/store/postgres/store_test.go
@@ -3,9 +3,7 @@ package postgres
 import (
 	"context"
 	"testing"
-	"time"
 
-	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/ory/dockertest"
 	"github.com/pkg/errors"
@@ -60,30 +58,4 @@ func newTestDatabase(tb testing.TB) (*Store, error) {
 	tb.Cleanup(tearDown)
 
 	return s, nil
-}
-
-func statusCheck(ctx context.Context, db *sqlx.DB) error {
-	// First check we can ping the database.
-	var pingError error
-	for attempts := 1; ; attempts++ {
-		pingError = db.Ping()
-		if pingError == nil {
-			break
-		}
-		time.Sleep(time.Duration(attempts) * 100 * time.Millisecond)
-		if ctx.Err() != nil {
-			return ctx.Err()
-		}
-	}
-
-	// Make sure we didn't timeout or be cancelled.
-	if ctx.Err() != nil {
-		return ctx.Err()
-	}
-
-	// Run a simple query to determine connectivity. Running this query forces a
-	// round trip through the database.
-	const q = `SELECT true`
-	var tmp bool
-	return db.QueryRowContext(ctx, q).Scan(&tmp)
 }
