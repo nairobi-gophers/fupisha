88855d090a510104e9fa5ddd07189962ee5dcc70
basebandit <evansonmwangi83@gmail.com>
2021-04-29 06:30:36 +0300

    refactor methods and their signatures

--- internal/store/postgres/user.go
+++ internal/store/postgres/user.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jmoiron/sqlx"
 
 	"github.com/nairobi-gophers/fupisha/internal/encoding"
-	"github.com/nairobi-gophers/fupisha/internal/store/model"
+	"github.com/nairobi-gophers/fupisha/internal/store"
 	"github.com/pkg/errors"
 )
 
@@ -18,44 +18,42 @@ type userStore struct {
 }
 
 //New creates a new user record.
-func (s userStore) New(ctx context.Context, name, email, password string) (model.User, error) {
-	tkn := encoding.GenUniqueID()
+func (s userStore) New(ctx context.Context, email, password string) (store.User, error) {
 
 	var now time.Time = time.Now()
 
-	user := model.User{
-		ID:                  encoding.GenUniqueID().String(),
-		Name:                name,
+	user := store.User{
+		ID:                  encoding.GenUniqueID(),
 		Email:               email,
 		Password:            password,
-		VerificationToken:   tkn,
+		VerificationToken:   encoding.GenUniqueID(),
 		VerificationExpires: now.Add(time.Minute * 60), //expires 60 mins later
 		CreatedAt:           now,
 		UpdatedAt:           now,
 	}
 
 	if err := user.HashPassword(); err != nil {
-		return model.User{}, err
+		return store.User{}, err
 	}
 
-	const q = `INSERT INTO users(id,name,email,password,verification_token,verification_expires,created_at,updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`
+	const q = `INSERT INTO users(id,email,password,verification_token,verification_expires,created_at,updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7)`
 
-	if _, err := s.db.ExecContext(ctx, q, user.ID, user.Name, user.Email, user.Password, user.VerificationToken, user.VerificationExpires, user.CreatedAt, user.UpdatedAt); err != nil {
-		return model.User{}, errors.Wrap(err, "inserting new user")
+	if _, err := s.db.ExecContext(ctx, q, user.ID, user.Email, user.Password, user.VerificationToken, user.VerificationExpires, user.CreatedAt, user.UpdatedAt); err != nil {
+		return store.User{}, errors.Wrap(err, "inserting new user")
 	}
 
 	return user, nil
 }
 
 //Get finds a user by id
-func (s userStore) Get(ctx context.Context, id string) (model.User, error) {
-	user := model.User{}
+func (s userStore) Get(ctx context.Context, id uuid.UUID) (store.User, error) {
+	user := store.User{}
 
 	const q = `SELECT id,name,email,api_key,created_at,verified FROM users WHERE id=$1`
 
 	if err := s.db.GetContext(ctx, &user, q, id); err != nil {
 		if err == sql.ErrNoRows {
-			return model.User{}, errors.New("not found")
+			return store.User{}, errors.New("not found")
 		}
 		return user, errors.Wrap(err, "retrieving user by id")
 	}
@@ -64,8 +62,8 @@ func (s userStore) Get(ctx context.Context, id string) (model.User, error) {
 }
 
 //GetByEmail retrieves an existing user with the given email.
-func (s userStore) GetByEmail(ctx context.Context, email string) (model.User, error) {
-	user := model.User{}
+func (s userStore) GetByEmail(ctx context.Context, email string) (store.User, error) {
+	user := store.User{}
 
 	const q = `SELECT id,name,email,api_key,created_at,verified FROM users WHERE email=$1`
 
@@ -77,8 +75,8 @@ func (s userStore) GetByEmail(ctx context.Context, email string) (model.User, er
 }
 
 //SetAPIKey sets the api key for the given user id.
-func (s userStore) SetAPIKey(ctx context.Context, id string, key uuid.UUID) error {
-	user := model.User{
+func (s userStore) SetAPIKey(ctx context.Context, id, key uuid.UUID) error {
+	user := store.User{
 		ID:     id,
 		APIKey: key,
 	}
