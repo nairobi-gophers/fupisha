f849dcea5b45cbe82cb153e762ef026e5a0ca849
basebandit <evansonmwangi83@gmail.com>
2021-05-04 21:10:41 +0300

    refactor: compare created url to the retrieved url

--- store/postgres/url_test.go
+++ store/postgres/url_test.go
@@ -2,7 +2,11 @@ package postgres
 
 import (
 	"context"
+	"reflect"
 	"testing"
+
+	"github.com/nairobi-gophers/fupisha/encoding"
+	"github.com/nairobi-gophers/fupisha/store"
 )
 
 func TestURL(t *testing.T) {
@@ -20,11 +24,42 @@ func TestURL(t *testing.T) {
 	}
 
 	originalURL := "http://highscalability.com/blog/2016/1/25/design-of-a-modern-cache.html"
-	shortURL := "https://fp.org/l2g4th9Urety2i"
 
-	_, err = s.Urls().New(ctx, u.ID, originalURL, shortURL)
+	param, err := encoding.GenUniqueParam("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6)
+
+	if err != nil {
+		t.Fatalf("failed to generate url param: %s", err)
+	}
+
+	url, err := s.Urls().New(ctx, u.ID, originalURL, param)
 	if err != nil {
 		t.Fatalf("failed to create url: %s", err)
 	}
 
+	want := store.URL{
+		ID:           url.ID,
+		Owner:        u.ID,
+		OriginalURL:  originalURL,
+		ShortenedURL: param,
+		CreatedAt:    url.CreatedAt,
+		UpdatedAt:    url.UpdatedAt,
+	}
+
+	got, err := s.Urls().GetByParam(ctx, param)
+	if err != nil {
+		t.Fatalf("failed to retrieve url param: %s", err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %+v\n want %+v\n", got, want)
+	}
+
+	url2, err := s.Urls().Get(ctx, url.ID)
+	if err != nil {
+		t.Fatalf("failed to retrieve url by id: %s", err)
+	}
+
+	if !reflect.DeepEqual(url, url2) {
+		t.Fatalf("got %+v\n want %+v\n", got, want)
+	}
 }
