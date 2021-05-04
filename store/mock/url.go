1ed8738472ce467f586e4a3c1f1ccfbf8bb96d18
basebandit <evansonmwangi83@gmail.com>
2021-05-04 21:15:59 +0300

    mock retrieval by param behaviour

--- store/mock/url.go
+++ store/mock/url.go
@@ -9,8 +9,9 @@ import (
 
 //URLStore is a mock implementation of store.urlStore
 type URLStore struct {
-	OnNew func(ctx context.Context, userID uuid.UUID, originalURL, shortenedURL string) (store.URL, error)
-	OnGet func(ctx context.Context, id uuid.UUID) (store.URL, error)
+	OnNew        func(ctx context.Context, userID uuid.UUID, originalURL, shortenedURL string) (store.URL, error)
+	OnGet        func(ctx context.Context, id uuid.UUID) (store.URL, error)
+	OnGetByParam func(ctx context.Context, param string) (store.URL, error)
 }
 
 func (u URLStore) New(ctx context.Context, userID uuid.UUID, originalURL, shortenedURL string) (store.URL, error) {
@@ -19,4 +20,8 @@ func (u URLStore) New(ctx context.Context, userID uuid.UUID, originalURL, shorte
 
 func (u URLStore) Get(ctx context.Context, id uuid.UUID) (store.URL, error) {
 	return u.OnGet(ctx, id)
+}
+
+func (u URLStore) GetByParam(ctx context.Context, param string) (store.URL, error) {
+	return u.OnGetByParam(ctx, param)
 }
