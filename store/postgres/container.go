e0279780a845f606e71bd508b888b8e6e4f051a9
basebandit <evansonmwangi83@gmail.com>
2021-05-29 16:56:59 +0300

    refactor: accept testing.T object and list all running containers

--- store/postgres/container.go
+++ store/postgres/container.go
@@ -1,9 +1,9 @@
 package postgres
 
 import (
-	"log"
 	"strconv"
 	"strings"
+	"testing"
 
 	"github.com/ory/dockertest/v3"
 	"github.com/ory/dockertest/v3/docker"
@@ -37,54 +37,52 @@ func NewPostgresqlContainer(pool *dockertest.Pool) *PostgresqlContainer {
 	return &PostgresqlContainer{pool: pool, opts: opts, imagename: "postgresql-testcontainer"}
 }
 
-func (container *PostgresqlContainer) Create() (*dockertest.Resource, error) {
-	if !isRunning(*container.pool, container.imagename) {
+func (container *PostgresqlContainer) Create(t *testing.T) {
 
-		dockerOpts := dockertest.RunOptions{
-			Repository: "postgres",
-			Tag:        "latest",
-			Env: []string{
-				"POSTGRES_USER=" + container.opts.User,
-				"POSTGRES_PASSWORD=" + container.opts.Password,
-				"POSTGRES_DB=" + container.opts.Database,
-			},
-			ExposedPorts: []string{strconv.Itoa(container.opts.Port)},
-			PortBindings: map[docker.Port][]docker.PortBinding{
-				docker.Port(strconv.Itoa(container.opts.Port)): {{HostIP: "0.0.0.0", HostPort: strconv.Itoa(container.opts.Port)}},
-			},
-			Name: container.imagename,
-		}
-
-		resource, err := container.pool.RunWithOptions(&dockerOpts, func(config *docker.HostConfig) {
-			// set AutoRemove to true so that stopped container goes away by itself
-			config.AutoRemove = true
-			config.RestartPolicy = docker.RestartPolicy{
-				Name: "no",
-			}
-		})
-		if err != nil {
-			log.Fatalf("Could not start resource (Postgresql Test Container): %s", err.Error())
-			return nil, err
-		}
-
-		return resource, nil
+	if isRunning(container.pool, container.imagename) {
+		t.Fatal(errors.New("container already exists and is running"))
 	}
-	return nil, errors.New("container already exists and is running")
+
+	dockerOpts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "latest",
+		Env: []string{
+			"POSTGRES_USER=" + container.opts.User,
+			"POSTGRES_PASSWORD=" + container.opts.Password,
+			"POSTGRES_DB=" + container.opts.Database,
+		},
+		ExposedPorts: []string{strconv.Itoa(container.opts.Port)},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			docker.Port(strconv.Itoa(container.opts.Port)): {{HostIP: "0.0.0.0", HostPort: strconv.Itoa(container.opts.Port)}},
+		},
+		Name: container.imagename,
+	}
+
+	resource, err := container.pool.RunWithOptions(&dockerOpts, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	container.resource = resource
 }
 
-func isRunning(pool dockertest.Pool, imagename string) bool {
+func isRunning(pool *dockertest.Pool, imagename string) bool {
 	dockerContainers, _ := pool.Client.ListContainers(docker.ListContainersOptions{
-		All: false,
+		All: true,
 	})
 
 	for _, dockerContainer := range dockerContainers {
 		for _, name := range dockerContainer.Names {
 			if strings.Contains(name, imagename) {
-				// fmt.Printf("%s imagename is running...", dockerContainer.imagename)
 				return true
 			}
 		}
 	}
-
 	return false
 }
