bb26fd08e1b57138c8887501da1e5e7de492670a
basebandit <evansonmwangi83@gmail.com>
2021-05-29 16:24:13 +0300

    add check for api version in request header here

--- api/v1/auth/routes.go
+++ api/v1/auth/routes.go
@@ -5,6 +5,7 @@ import "github.com/go-chi/chi"
 //Router provides necessary routes for password restricted authentication flow.
 func (rs *Resource) Router() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(CheckAPI)
 	r.Post("/signup", rs.HandleSignup)
 	r.Post("/login", rs.HandleLogin)
 	return r
