1196a74e56a720f14da75ab0f2c922133ef3c575
basebandit <evansonmwangi83@gmail.com>
2021-05-30 13:59:21 +0300

    refactor: rename variable to match renamed url table's short_url column

--- store/url_model.go
+++ store/url_model.go
@@ -8,11 +8,11 @@ import (
 
 //URL contains all the related info about the shortened url.
 type URL struct {
-	ID           uuid.UUID `db:"id"`
-	Owner        uuid.UUID `db:"owner"`
-	OriginalURL  string    `db:"original_url"`
-	ShortenedURL string    `db:"short_url"`
-	VisitCount   *int      `db:"visit_count,omitempty"`
-	CreatedAt    time.Time `db:"created_at"`
-	UpdatedAt    time.Time `db:"updated_at"`
+	ID                uuid.UUID `db:"id"`
+	Owner             uuid.UUID `db:"owner"`
+	OriginalURL       string    `db:"original_url"`
+	ShortenedURLParam string    `db:"short_url_param"`
+	VisitCount        *int      `db:"visit_count,omitempty"`
+	CreatedAt         time.Time `db:"created_at"`
+	UpdatedAt         time.Time `db:"updated_at"`
 }
