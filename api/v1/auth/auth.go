1054b9e0731a6499f2b6ada9c1a307c8d209b7e3
basebandit <evansonmwangi83@gmail.com>
2021-06-18 11:44:11 +0300

    refactor: define the mailer as a dependency for this package

--- api/v1/auth/auth.go
+++ api/v1/auth/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/nairobi-gophers/fupisha/config"
+	"github.com/nairobi-gophers/fupisha/provider"
 	"github.com/nairobi-gophers/fupisha/store"
 )
 
@@ -11,13 +12,15 @@ import (
 type Resource struct {
 	Store  store.Store
 	Config *config.Config
+	Mailer *provider.Mailer
 }
 
 //NewResource returns a configured authentication resource.
-func NewResource(store store.Store, cfg *config.Config) *Resource {
+func NewResource(store store.Store, cfg *config.Config, mailer *provider.Mailer) *Resource {
 	return &Resource{
 		Store:  store,
 		Config: cfg,
+		Mailer: mailer,
 	}
 }
 
