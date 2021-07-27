44c4dd818387055d1e3a7407ebce35301646f7e2
basebandit <evansonmwangi83@gmail.com>
2021-07-27 13:46:09 +0300

    add token verification route and regroup routes requiring api version header check

--- api/v1/auth/routes.go
+++ api/v1/auth/routes.go
@@ -5,8 +5,11 @@ import "github.com/go-chi/chi"
 //Router provides necessary routes for password restricted authentication flow.
 func (rs *Resource) Router() *chi.Mux {
 	r := chi.NewRouter()
-	r.Use(CheckAPI)
-	r.Post("/signup", rs.HandleSignup)
-	r.Post("/login", rs.HandleLogin)
+	r.Get("/verify", rs.HandleVerify) //verify verification token using url query i.e /auth/verify?v="assgsggsghahs563782"
+	r.Group(func(r chi.Router) {
+		r.Use(CheckAPI)
+		r.Post("/signup", rs.HandleSignup)
+		r.Post("/login", rs.HandleLogin)
+	})
 	return r
 }
