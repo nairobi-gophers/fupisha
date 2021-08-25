4d27dcb0c13e23d69bccc8e873a0c03252564200
basebandit <evansonmwangi83@gmail.com>
2021-08-25 23:45:57 +0300

    landing page

---
+++ web/src/pages/LandingPage.vue
@@ -0,0 +1,16 @@
+!<template>
+  <div>
+    <navbar />
+  </div>
+</template>
+<script>
+import Navbar from "../components/Navbar.vue";
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: { Navbar },
+  name: "LandingPage",
+  component: {
+    Navbar,
+  },
+};
+</script>
