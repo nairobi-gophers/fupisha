814df9ae9be3713e72452fa46c6086d1ad1dffe3
basebandit <evansonmwangi83@gmail.com>
2021-08-25 23:47:14 +0300

    init vue app

---
+++ web/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="leading-normal tracking-normal text-white gradient">
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+import LandingPage from "./components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    LandingPage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
