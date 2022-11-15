f9a088f79d079c8700bcb8f67a4806a7db93c8ef
Capricorn <evansonmwangi83@gmail.com>
2022-11-15 15:27:16 +0300

    refactor store api to avoid methods with similar names in a bid to avoid name clash when embedding interfaces in the store abstraction(interface)

--- store/store.go
+++ store/store.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/gofrs/uuid"
-	"github.com/nairobi-gophers/fupisha/store"
 )
 
 // Store is composes all the different store abstractions into one single abstraction.
@@ -15,18 +14,18 @@ type Store interface {
 
 // UserStore is a user data store interface.
 type UserStore interface {
-	NewUser(ctx context.Context, email, password string) (store.User, error)
-	GetUserByID(ctx context.Context, id uuid.UUID) (store.User, error)
-	GetUserByEmail(ctx context.Context, email string) (store.User, error)
-	GetUserByVerificationToken(ctx context.Context, token uuid.UUID) (store.User, error)
+	NewUser(ctx context.Context, email, password string) (User, error)
+	GetUserByID(ctx context.Context, id uuid.UUID) (User, error)
+	GetUserByEmail(ctx context.Context, email string) (User, error)
+	GetUserByVerificationToken(ctx context.Context, token uuid.UUID) (User, error)
 	SetUserAPIKey(ctx context.Context, id, key uuid.UUID) error
 	SetUserVerified(ctx context.Context, id uuid.UUID) error
 }
 
 // URLStore is a url data store interface.
 type URLStore interface {
-	NewURL(ctx context.Context, userID uuid.UUID, originalURL, shortenedURL string) (store.URL, error)
-	GetURLByID(ctx context.Context, id uuid.UUID) (store.URL, error)
-	GetURLByParam(ctx context.Context, param string) (store.URL, error)
-	GetURLByLongStr(ctx context.Context, longURL string) (store.URL, error)
+	NewURL(ctx context.Context, userID uuid.UUID, originalURL, shortenedURL string) (URL, error)
+	GetURLByID(ctx context.Context, id uuid.UUID) (URL, error)
+	GetURLByParam(ctx context.Context, param string) (URL, error)
+	GetURLByLongStr(ctx context.Context, longURL string) (URL, error)
 }
